23a216ee6295871e9330d89fee5d981706a4db67
CodeChd <Drowzyking1126@gmail.com>
2023-10-19 20:50:06 +0800

    Setup essential dependenncies and config's

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-5xl text-royalBlue'>Goodmorning'</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      Ins: "Instrument Sans, sans-serif",
+    },
+    extend: {
+      colors: {
+        royalBlue: "#633CFF",
+        lavender: "#BEADFF",
+        babyPowder: "#EFEBFF",
+        richBlack: "#333333",
+        mediumGrey: "#737373",
+        lightGrey: "#D9D9D9",
+        snow: "#FAFAFA",
+        white: "#FFFFFF",
+        crimson: "#FF3939",
+      },
+      fontSize: {
+        "h-m-b": "32px",
+        "h-s-b": "16px",
+        "b-m": "16px",
+        "b-s": "12px",
+      },
+    },
+  },
+  plugins: [],
+};
