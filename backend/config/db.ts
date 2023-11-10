891c60f190448b23fa1710208ae29b7a216e4609
CodeChd <Drowzyking1126@gmail.com>
2023-11-10 17:14:13 +0800

    Ready for deploy && Refactoring  to make it more readable

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // for docker
 // const env = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/devlinks";
 
-const env = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/devlinks";
+const env = process.env.MONGO_URL as string || process.env.MONGO_URI as string;
 async function connectDB() {
   try {
     await mongoose.connect(env as string);
