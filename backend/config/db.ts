de6f52bbb18ec5478d470d1f887bce3068399080
CodeChd <Drowzyking1126@gmail.com>
2023-11-11 01:11:43 +0800

    Ready for deploy

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 // for docker
 // const env = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/devlinks";
 
-const env = process.env.MONGO_URL as string || process.env.MONGO_URI as string;
+const env =
+  (process.env.MONGO_URL as string) ||
+  (process.env.MONGO_URI as string) ||
+  "mongodb://127.0.0.1:27017/devlinks";
 async function connectDB() {
   try {
-    await mongoose.connect(env as string);
+    await mongoose.connect(env);
   } catch (error) {
     console.log(error);
     process.exit(1);
--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo "Waiting for MongoDB to start..."
-./wait-for db:27017 
 
 
 echo "Starting the server..."
