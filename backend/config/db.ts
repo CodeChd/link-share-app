0200e1b3aebb24a0c72a3fa76e95a5d3e0432682
CodeChd <Drowzyking1126@gmail.com>
2023-11-11 12:48:58 +0800

    Ready for deploy with Docker

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
-// for docker
-// const env = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/devlinks";
-
 const env =
   (process.env.MONGO_URL as string) ||
-  (process.env.MONGO_URI as string) ||
+  (process.env.MONGO_URI as string) || 
   "mongodb://127.0.0.1:27017/devlinks";
 async function connectDB() {
   try {
--- backend/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Waiting for MongoDB to start..."
-
-
-echo "Starting the server..."
-npm start 
