bcceaddfbb5682582579d83b0278f536d2497f6e
CodeChd <Drowzyking1126@gmail.com>
2023-11-11 13:11:38 +0800

    Ready for deploy with Docker

--- backend/server.ts
+++ backend/server.ts
@@ -27,9 +27,15 @@ app.use("/api/preview", previewRoute);
 if (process.env.NODE_ENV === ("production" as string)) {
   const __dirname = path.resolve();
 
-  app.use(express.static(path.join(__dirname, "frontend-dist")));
+  // for docker
+  // app.use(express.static(path.join(__dirname, "frontend-dist")));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "frontend-dist", "index.html"));
+  // });
+
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend-dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 } else {
   app.get("/", (req: Request, res: Response) => {
